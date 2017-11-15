1b805a722d6b7e46c8eb8d68dd6c4a0ec5152f2c
steve <syanney@hotmail.com>
2017-11-15 19:46:54 +0000

    upgraded to use angular 5

--- Internal.Web.UI/wwwroot/dist/rxjs-bundle.js
+++ Internal.Web.UI/wwwroot/dist/rxjs-bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([4],{38:function(e,t,c){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),c(38)}},[38]);
+webpackJsonp([4],{41:function(e,t,c){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),c(41)}},[41]);
 //# sourceMappingURL=rxjs-bundle.js.map
