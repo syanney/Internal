e584a7d023d6d72005157ca48f79ac8fabc24cc8
steve <syanney@hotmail.com>
2018-02-02 21:45:14 +0000

    commit

--- Internal.SqlDataSource/Scripts.sql
+++ Internal.SqlDataSource/Scripts.sql
@@ -1,9 +1,21 @@
 
 
-CREATE TABLE [dbo].[Employee]
-(
-    [Id] [int] IDENTITY(1,1) NOT NULL,
-    [FirstName] nvarchar(100) NOT NULL,
-    [LastName] nvarchar(100) NOT NULL,    
- CONSTRAINT [PK_Employee] PRIMARY KEY (    [Id])
-)
+IF DB_ID('Internal') IS NULL
+BEGIN
+	CREATE DATABASE Internal
+END
+GO
+
+USE Internal
+
+IF NOT EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Employee')
+BEGIN
+	CREATE TABLE [dbo].[Employee]
+	(
+		[Id] [int] IDENTITY(1,1) NOT NULL,
+		[FirstName] NVARCHAR(100) NOT NULL,
+		[MiddleName] NVARCHAR(100) NULL,
+		[LastName] NVARCHAR(100) NOT NULL,    
+	 CONSTRAINT [PK_Employee] PRIMARY KEY (    [Id])
+	)
+END
